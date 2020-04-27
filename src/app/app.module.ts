ca625f47be63d52e4cafc7c801e7968a835da583
govardhana <42324366+naidugova@users.noreply.github.com>
2020-04-27 23:20:33 +0530

    Added the components required for blog app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
